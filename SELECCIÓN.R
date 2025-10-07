d8b564cf7942141b6168636769c9b07c3c72c245
Humadro <165678923+Humadro@users.noreply.github.com>
2025-10-07 18:34:26 +0200

    Add files via upload

--- SELECCIÓN.R
+++ SELECCIÓN.R
@@ -8,6 +8,7 @@ library(car)
 library(ggplot2)
 library(scales)
 library(patchwork)
+library(dplyr)
 
 #====================================================================
 # S'HA DE CARREGAR TAMBÉ EL RDATA DE IMPUTACIÓ ======================
@@ -31,11 +32,37 @@ num_panel <- wrap_plots(num_plots, ncol = 3) +
 num_panel
 
 
+# Kruskal - wallis =========================================
+
+kruskal_results <- lapply(numeric_vars, function(var) {
+  # fórmula: variable ~ Exited
+  formula <- as.formula(paste("Exited ~ ", var))
+  test <- kruskal.test(formula, data = data_imputed_MICE)
+  
+  data.frame(
+    Variable = var,
+    Statistic = test$statistic,
+    p_value = test$p.value,
+    stringsAsFactors = FALSE
+  )
+})
+
+kruskal_table <- do.call(rbind, kruskal_results)
+kruskal_results = as.data.frame(kruskal_results)
+kruskal_results %>% select(Variable, p_value) %>% where(p_value > 0.05)
+
+# ============================
+# 📊 3️⃣ Ordenar por p-valor
+# ============================
+kruskal_table <- kruskal_table[order(kruskal_table$p_value), ]
+
+kruskal_table
+
 # Barplots 
 cat_vars <- setdiff(names(data_imputed_MICE)[sapply(data_imputed_MICE, is.factor)], "Exited")
 
-cat_plots <- lapply(cat_vars, function(var) {
-  ggplot(data_imputed_MICE, aes(x = .data[[var]], fill = Exited)) +
+cat_plots <- lapply(cat_vars[-4], function(var) {
+  ggplot(data_imputed_MICE[,-7], aes(x = .data[[var]], fill = Exited)) +
     geom_bar(position = "fill") +
     scale_y_continuous(labels = percent_format()) +
     labs(title = var, x = NULL, y = NULL) +
@@ -50,6 +77,14 @@ cat_panel <- wrap_plots(cat_plots, ncol = 3) +
 
 print(cat_panel)
 
+data_imputed_MICE$IsActiveMember = as.factor(data_imputed_MICE$IsActiveMember)
+
+ggplot(data_imputed_MICE, aes(x = IsActiveMember, y = Balance, fill = Exited)) +
+    geom_boxplot() +
+    labs(title = var, x = NULL, y = NULL) +
+    theme_minimal() +
+    theme(legend.position = "none",
+          plot.title = element_text(hjust = 0.5, size = 10))
 # GLM
 formula_glm <- as.formula(paste("Exited ~", paste(setdiff(names(data), "Exited"), collapse = " + ")))
 
@@ -88,5 +123,4 @@ lasso_coefs$Variable <- rownames(lasso_coefs)
 lasso_coefs <- lasso_coefs[order(-abs(lasso_coefs$Estimate)), ]  # ordenar por magnitud
 
 print(lasso_coefs)
-
 
